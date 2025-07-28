682fd9f095e403219b4498af86fa854ca3dda4ce
Shravani1603 <149895354+Shravani1603@users.noreply.github.com>
2025-07-28 23:38:25 +0530

    Update app.py

--- web3/app.py
+++ web3/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/health')
+def health_check():
+    return 'OK', 200
+
 @app.route('/web3')
 def home():
     return "Hello from Web3 - Python Flask"
