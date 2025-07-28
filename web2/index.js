be2f7830c42f27c58f5fc766e318fa7d050b1749
Shravani1603 <149895354+Shravani1603@users.noreply.github.com>
2025-07-28 23:39:31 +0530

    Update index.js

--- web2/index.js
+++ web2/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5501;
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.get('/web2', (req, res) => {
   res.send('Hello from Web2 - Node.js!');
 });
