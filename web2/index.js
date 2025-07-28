a804c884d52f228381da8cb3d6b99a6defd8e082
Shravani1603 <149895354+Shravani1603@users.noreply.github.com>
2025-07-28 10:53:19 +0530

    Create index.js

---
+++ web2/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5501;
+
+app.get('/web2', (req, res) => {
+  res.send('Hello from Web2 - Node.js!');
+});
+
+app.listen(port, () => {
+  console.log(`Web2 running on http://localhost:${port}`);
+});
